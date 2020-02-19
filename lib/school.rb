14ddca976e94068013c02c7bad287f794b10063a
Nicholas Patel <np4795@yahoo.com>
2020-02-19 20:35:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,21 +1,30 @@
+class School
 
-  # code here!
-  class School
-	attr_accessor :roster, :name, :grade 
-	attr_reader :school 
-	
-	def initialize(school)
-    @school = school
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
   end
-  
+
   def roster
-  	roster = {}
-  end 
-  
-  def add_student(name,grade)
-  	roster[grade] = []
-  	roster[grade] << name
-  		
+    @roster
   end
 
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
